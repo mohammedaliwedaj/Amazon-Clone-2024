f8276096dd8121cbde5bcc713f05d1847bb75329
Mohammed Ali <mohammedaliwedaj@gmail.com>
2024-09-07 23:09:04 +0300

    Carousel

--- amazon-clone-2024/src/App.jsx
+++ amazon-clone-2024/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/CarouselEffect'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header/>
+      <CarouselEffect/>
     </>
   )
 }
